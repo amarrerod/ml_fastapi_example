0bff51d2c0f6bbb1845da476636d5293056a582d
amarrerod <alemarrerodiaz@gmail.com>
2021-06-18 15:38:18 +0100

    🚀 Project completed

--- ml_fastapi_example/app.py
+++ ml_fastapi_example/app.py
@@ -1,8 +1,11 @@
 
 import uvicorn
 from fastapi import FastAPI
+from model import IrisModel, IrisSpecies
+
 
 app = FastAPI()
+classifier = IrisModel()
 
 
 @app.get('/')
@@ -21,6 +24,20 @@ def get_name(name: str):
         where a greeting message is returned
     '''
     return {'message': f'Hello, {name}'}
+
+
+@app.post('/predict')
+def predict_species(iris: IrisSpecies):
+    data = iris.dict()
+    pred, prob = classifier.predict(data['sepal_length'],
+                                    data['sepal_width'],
+                                    data['petal_length'],
+                                    data['petal_width']
+                                    )
+    return {
+        'prediction': pred,
+        'probability': prob
+    }
 
 
 if __name__ == '__main__':
