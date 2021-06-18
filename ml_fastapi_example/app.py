5bb9b7ff95e36c4ab1bea768bcbed5359784d3c5
amarrerod <alemarrerodiaz@gmail.com>
2021-06-18 15:24:45 +0100

    📝 Includes documentation

--- ml_fastapi_example/app.py
+++ ml_fastapi_example/app.py
@@ -7,11 +7,19 @@ app = FastAPI()
 
 @app.get('/')
 def index():
+    '''
+        Index page of the application.
+        Returns a Hello World message
+    '''
     return {'message', 'Hello, world!'}
 
 
 @app.get('/{name}')
 def get_name(name: str):
+    '''
+        This is the get_name endpoint
+        where a greeting message is returned
+    '''
     return {'message': f'Hello, {name}'}
 
 
